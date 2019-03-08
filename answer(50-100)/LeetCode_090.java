3d80d204e48747d243b1d822a4f43ffcadfd7d0b
YunPengZ <Zhu_freedom@aliyun.com>
2019-03-08 15:45:07 +0800

    update backtrack's problem

--- answer(50-100)/LeetCode_090.java
+++ answer(50-100)/LeetCode_090.java
@@ -1,21 +1,23 @@
 package packag.start.from081;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode_090 {
 	public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result  = new ArrayList<List<Integer>>();
+        Arrays.parallelSort(nums);
 		backtrack(result,new ArrayList<Integer>(),nums,0);
 		return result;
     }
 
 	private void backtrack(List<List<Integer>> result, ArrayList<Integer> arrayList, int[] nums,int start) {
-		result.add(arrayList);
+		result.add(new ArrayList<Integer>(arrayList));
 		for(int i = start;i<nums.length;i++) {
 			if(i>start&&nums[i]==nums[i-1])continue;
 			arrayList.add(nums[i]);
-			backtrack(result, arrayList, nums, start+1);
+			backtrack(result, arrayList, nums, i+1);
 			arrayList.remove(arrayList.size()-1);//删除已添加的元素 遍历更多可能
 		}
 	}	
